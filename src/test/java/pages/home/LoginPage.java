2d5a07cf5ec0d641b13c0e8511ffedd335d75c43
davibfarias <davi.bfarias.santos@gmail.com>
2023-10-15 01:16:55 -0300

    ct_01_authenctication - Finished test case development

---
+++ src/test/java/pages/home/LoginPage.java
@@ -0,0 +1,38 @@
+package pages.home;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import pages.base.PageBase;
+
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.$;
+
+public class LoginPage extends PageBase {
+
+    // Elements
+
+    // I used the property type in this locator because "b3-" in ID looks generic, may can change in the future
+    // And others inputs can be added in the same page later, so "type" looked more secure to me
+    private final SelenideElement inputEmail = $(byAttribute("type", "email"));
+    private final SelenideElement inputPassword = $(byAttribute("type", "password"));
+
+    // In deep analysis byText and byTagAndText uses Xpath to locate the object, but not using the entire path, it gets the page elements and find the element with the text
+    // It appears over processing, but it's very clean, fast and resilient if the position of the object changes
+    private final SelenideElement buttonLogin = $(byTextCaseInsensitive("Log in"));
+
+    // Methods
+    public void fillEmailField(String email) {
+        inputEmail.should(Condition.enabled);
+        inputEmail.setValue(email);
+    }
+
+    public void fillPasswordField(String password) {
+        inputPassword.should(Condition.enabled);
+        inputPassword.setValue(password);
+    }
+
+    public void pressLoginButton(){
+        saveScreenshot();
+        buttonLogin.click();
+    }
+}
---
+++ src/test/java/stepdefs/geral/GeralStepdefs.java
@@ -0,0 +1,19 @@
+package stepdefs.geral;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import helpers.GlobalParameters;
+import io.cucumber.java.en.Given;
+
+public class GeralStepdefs {
+
+    // Steps
+    @Given("the OutSystems training website is opened")
+    public void accessLoginOption() {
+        Configuration.holdBrowserOpen = true; // Used as true for help in development
+        Configuration.headless = true; // Used for CLI or CI/CD environments
+        Configuration.timeout = 30000;
+        Selenide.open(GlobalParameters.getInstance().getProperty("base_url_training"));
+        Selenide.webdriver().driver().getWebDriver().manage().window().maximize();
+    }
+}
